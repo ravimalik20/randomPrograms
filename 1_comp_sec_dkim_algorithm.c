7740918d555e664e3f1381418690b094b12c6230
Ravi Malik <ravimalik2364@gmail.com>
2017-11-25 01:41:18 -0500

    Code refactor. Timing of operations is also calculated.

--- 1_comp_sec_dkim_algorithm.c
+++ 1_comp_sec_dkim_algorithm.c
@@ -11,122 +11,148 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+#include <dirent.h>
 
 #define COMMAND_LEN_MAX 2048
+#define FILE_NAME_MAX 100
 #define COMMAND_BLOCKS_MAX 100
 #define COMMAND_BLOCKS_LEN_MAX 50
 
+#define TIME_ELAPSED(start, end) ((double) ((end) - (start))) / CLOCKS_PER_SEC
+
 void execute(char *command);
+void keygen(int keylen);
+void sign(int sha, int rsa, char *file_name);
+void verify(int sha, int rsa, char *file_name);
+void cleanup(void);
+
+char *passphrase = "sample_passphrase";
+char *rsa_priv_1024 = "rsaprivatekey1024.pem";
+char *rsa_pub_1024 = "rsapublickey1024.pem";
+char *rsa_priv_2048 = "rsaprivatekey2048.pem";
+char *rsa_pub_2048 = "rsapublickey2048.pem";
 
 int main(void)
 {
-	char command_keygen_1024[COMMAND_LEN_MAX], command_keygen_2048[COMMAND_LEN_MAX];
-	char *passphrase = "sample_passphrase";
-	char *rsa_priv_1024 = "rsaprivatekey1024.pem";
-	char *rsa_pub_1024 = "rsapublickey1024.pem";
-	char *rsa_priv_2048 = "rsaprivatekey2048.pem";
-	char *rsa_pub_2048 = "rsapublickey2048.pem";
+	clock_t start, end;
+	
+	printf ("##### Generating RSA Keys #####\n");
 
-	sprintf(command_keygen_1024, "openssl genrsa -aes128 -passout pass:%s -out %s 1024 &&\
-		openssl rsa -in %s -outform PEM -pubout -passin pass:%s -out %s",
-		passphrase, rsa_priv_1024, rsa_priv_1024, passphrase, rsa_pub_1024);
+	keygen (1024);
+	keygen (2048);
 
-	sprintf(command_keygen_2048, "openssl genrsa -aes128 -passout pass:%s -out %s 2048 &&\
-		openssl rsa -in %s -outform PEM -pubout -passin pass:%s -out %s",
-		passphrase, rsa_priv_2048, rsa_priv_2048, passphrase, rsa_pub_2048);
+	printf ("Complete.\n\n");
 
-	printf ("Generating RSA 1024 key pair.\n");
-	system(command_keygen_1024);
-	printf ("Complete\n");
+	char *file_name;
 
-	printf ("Generating RSA 1024 key pair.\n");
-	system(command_keygen_2048);
-	printf ("Complete\n");
+	DIR *dir;
+	struct dirent *ent;
 
-	int i;
-	char file_name[20];
+	if ((dir = opendir(".")) == NULL) {
+		fprintf(stderr, "Could not open current directory.\n");
 
-	for (i = 1 ; i <= 10 ; i++) {
-		sprintf(file_name, "msg%d.txt", i);
-
-		printf ("Processing for file: %s\n", file_name);
-
-		char command_sha_128_digest_1024[COMMAND_LEN_MAX];
-		char command_sha_256_digest_1024[COMMAND_LEN_MAX];
-		char command_sha_128_digest_2048[COMMAND_LEN_MAX];
-		char command_sha_256_digest_2048[COMMAND_LEN_MAX];
-
-		sprintf(command_sha_128_digest_1024, "openssl dgst -sha1 -passin pass:%s -sign %s -out sign_128_1024 %s",
-			passphrase, rsa_priv_1024, file_name);
-
-		sprintf(command_sha_256_digest_1024, "openssl dgst -sha256 -passin pass:%s -sign %s -out sign_256_1024 %s",
-			passphrase, rsa_priv_1024, file_name);
-
-		sprintf(command_sha_128_digest_2048, "openssl dgst -sha1 -passin pass:%s -sign %s -out sign_128_2048 %s",
-			passphrase, rsa_priv_2048, file_name);
-
-		sprintf(command_sha_256_digest_2048, "openssl dgst -sha256 -passin pass:%s -sign %s -out sign_256_2048 %s",
-			passphrase, rsa_priv_2048, file_name);
-
-		printf ("Generating signatures:\n");
-
-		system(command_sha_128_digest_1024);
-		system(command_sha_256_digest_1024);
-		system(command_sha_128_digest_2048);
-		system(command_sha_256_digest_2048);
-
-		printf ("Complete.\n");
-
-		char command_verify_sha_128_digest_1024[COMMAND_LEN_MAX];
-		char command_verify_sha_256_digest_1024[COMMAND_LEN_MAX];
-		char command_verify_sha_128_digest_2048[COMMAND_LEN_MAX];
-		char command_verify_sha_256_digest_2048[COMMAND_LEN_MAX];
-
-		sprintf(command_verify_sha_128_digest_1024, "openssl dgst -sha1 -verify %s -signature %s %s",
-			rsa_pub_1024, "sign_128_1024", file_name);
-
-		sprintf(command_verify_sha_256_digest_1024, "openssl dgst -sha256 -verify %s -signature %s %s",
-			rsa_pub_1024, "sign_256_1024", file_name);
-
-		sprintf(command_verify_sha_128_digest_2048, "openssl dgst -sha1 -verify %s -signature %s %s",
-			rsa_pub_2048, "sign_128_2048", file_name);
-
-		sprintf(command_verify_sha_256_digest_2048, "openssl dgst -sha256 -verify %s -signature %s %s",
-			rsa_pub_2048, "sign_256_2048", file_name);
-
-		printf ("Verifying signatures.\n");
-
-		system(command_verify_sha_128_digest_1024);
-		system(command_verify_sha_256_digest_1024);
-		system(command_verify_sha_128_digest_2048);
-		system(command_verify_sha_256_digest_2048);
-
-		printf ("Complete\n");
+		return -1;
 	}
+
+	while ((ent = readdir (dir)) != NULL) {
+		file_name = ent->d_name;
+
+		if (strcmp (file_name, ".") == 0 || strcmp (file_name, "..") == 0)
+			continue;
+
+		printf ("##### Generating signatures for file: %s #####\n", file_name);
+
+		sign (128, 1024, file_name);
+		sign (256, 1024, file_name);
+		sign (128, 2048, file_name);
+		sign (256, 2048, file_name);
+
+		printf ("Complete.\n\n");
+
+		printf ("Verifying signatures\n");
+
+		verify (128, 1024, file_name);
+		verify (256, 1024, file_name);
+		verify (128, 2048, file_name);
+		verify (256, 2048, file_name);
+
+		printf ("Complete.\n\n");
+	}
+
+	printf ("Cleanup.\n");
+	cleanup();
 
 	return 0;
 }
 
-void execute(char *command)
+void keygen(int keylen)
 {
-	char *command_execve[COMMAND_BLOCKS_MAX];
+	char command[COMMAND_LEN_MAX];
+	clock_t start, end;
 
-	char cmd[100];
-	strcpy(command, cmd);
+	printf ("Generating RSA keys for key lenght: %d\n", keylen);
 
-	char *command_name;
+	char *rsa_priv = (keylen == 1024 ? rsa_priv_1024 : rsa_priv_2048);
+	char *rsa_pub = (keylen == 1024 ? rsa_pub_1024 : rsa_pub_2048);
 
-	int i = 0;
-	const char del[2] = " ";
-	char *token = strtok (cmd, del);
+	sprintf(command, "openssl genrsa -aes128 -passout pass:%s -out %s %d &&\
+		openssl rsa -in %s -outform PEM -pubout -passin pass:%s -out %s",
+		passphrase, rsa_priv, keylen, rsa_priv, passphrase,
+		rsa_pub);
 
-	command_name = token;
+	start = clock();
+	system (command);
+	end = clock();
 
-    while (token != NULL) {
-        token = strtok (NULL, del);
+	printf ("Done.\n");
+	printf ("Time Elapsed: %lf sec\n", TIME_ELAPSED(start, end));
+}
 
-		command_execve[i++] = token;
-    }
+void sign(int sha, int rsa, char *file_name)
+{
+	char command[COMMAND_LEN_MAX];
+	clock_t start, end;
 
-	execv(command_name, command_execve);
+	printf ("Creating signature for Keylen: %d and SHA%d\n", rsa, sha);
+
+	sprintf(command, "openssl dgst -sha%d -passin pass:%s -sign %s -out sign_%d_%d %s",
+		(sha == 128 ? 1 : 256), passphrase, (rsa == 1024 ? rsa_priv_1024 : rsa_priv_2048),
+		sha, rsa, file_name);
+
+	start = clock();
+	system (command);
+	end = clock();
+
+	printf ("Done\n");
+	printf ("Time Elapsed: %lf sec\n", TIME_ELAPSED(start, end));
+}
+
+void cleanup(void)
+{
+	char command[COMMAND_LEN_MAX];
+
+	sprintf (command, "rm sign_* %s %s %s %s", rsa_priv_1024, rsa_pub_1024,
+		rsa_priv_2048, rsa_pub_2048);
+
+	system (command);
+}
+
+void verify(int sha, int rsa, char *file_name)
+{
+	char command[COMMAND_LEN_MAX];
+	clock_t start, end;
+
+	printf ("Verifying signature for Keylen: %d and SHA%d\n", rsa, (sha == 128 ? 1 : 256));
+
+	sprintf(command, "openssl dgst -sha%d -verify %s -signature sign_%d_%d %s",
+		(sha == 128 ? 1 : 256), (rsa == 1024 ? rsa_pub_1024 : rsa_pub_2048),
+		sha, rsa, file_name);
+
+	start = clock();
+	system (command);
+	end = clock();
+
+	printf ("Done\n");
+	printf ("Time Elapsed: %lf sec\n", TIME_ELAPSED(start, end));
 }
