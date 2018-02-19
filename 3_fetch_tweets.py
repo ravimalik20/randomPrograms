9cf251a9825b20dbdc7b320d88ac0e7d78c409a1
Ravi Malik <ravimalik2364@gmail.com>
2018-02-19 17:11:52 -0500

    Tweets an be fetched in batches with persistence.

--- 3_fetch_tweets.py
+++ 3_fetch_tweets.py
@@ -1,6 +1,8 @@
 import json
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from threading import Thread, Condition
+import pickle
+import os
 
 ACCESS_TOKEN = '961360036525113345-Sh9wqziTRPqHnQvBAFqR6X7Ie2W9eV1'
 ACCESS_SECRET = 'y1fluOM1sp6pJ4Wyc7L3pDwkEYByIQZW7xcCgZj23QJIf'
@@ -9,7 +11,9 @@ CONSUMER_SECRET = 'c4os7vhVH33UYNYgt6mHAcbrSECAR8ZZugFKeyRmf3pFriXbjb'
 
 class TweetFetchWorker(Thread):
 	TWEETS_PER_REQUEST = 99
-	MAX_TWEETS = 20000
+	MAX_TWEETS = 200
+	TWEETS_TEMP_FILE = "tweets.pickle"
+	TWEETS_OUTPUT_FILE = "tweets.json"
 
 	def __init__(self, access_token, access_secret, consumer_key,
 		consumer_secret, query):
@@ -27,47 +31,78 @@ class TweetFetchWorker(Thread):
 		self.query = query
 
 		self.tweets = dict()
+		self.__loadTweets()
+
+		if len(self.tweets) <= TweetFetchWorker.MAX_TWEETS:
+			self.max_tweets = TweetFetchWorker.MAX_TWEETS
+		else:
+			self.max_tweets = len(self.tweets) * 2
 
 	def run(self):
 		try:
 			wait = Condition()
 
-			while len(self.tweets) <= TweetFetchWorker.MAX_TWEETS:
+			while len(self.tweets) <= self.max_tweets:
 				with wait:
-					if len(self.tweets.keys()) > 0:
-						data = self.twitter.search.tweets(q = self.query,
-							result_type='recent', lang = 'en',
-							count = TweetFetchWorker.TWEETS_PER_REQUEST,
-							max_id = min(self.tweets.keys())
-						)
-					else:
-						data = self.twitter.search.tweets(q = self.query,
-							result_type='recent', lang = 'en',
-							count = TweetFetchWorker.TWEETS_PER_REQUEST
-						)
-
-					for tweet in data['statuses']:
-						obj = {
-							'id': tweet['id'],
-							'text': tweet['text'],
-							'geo': tweet['geo'],
-							'coordinates': tweet['coordinates'],
-							'created_at': tweet['created_at'],
-							'location': tweet['user']['location'],
-						}
-
-						self.tweets[tweet['id']] = obj
+					data = self.__getTweets()
+					self.__pushTweets(data)
 
 					wait.wait(2)
-
+		except KeyboardInterrupt:
+			self.__saveTweets()
 		finally:
-			final_data = list(self.tweets.values())
-			f = open("tweets.json", "w")
+			self.__saveTweets()
 
-			f.write(json.dumps(final_data, sort_keys = True, indent = 4))
+	def __pushTweets(self, data):
+		for tweet in data['statuses']:
+			if tweet['retweeted'] == True:
+				continue
+
+			obj = {
+				'id': tweet['id'],
+				'text': tweet['text'],
+				'geo': tweet['geo'],
+				'coordinates': tweet['coordinates'],
+				'created_at': tweet['created_at'],
+				'location': tweet['user']['location'],
+			}
+
+			self.tweets[tweet['id']] = obj
+
+	def __getTweets(self):
+		if len(self.tweets.keys()) > 0:
+			data = self.twitter.search.tweets(q = self.query,
+				result_type='recent', lang = 'en',
+				count = TweetFetchWorker.TWEETS_PER_REQUEST,
+				max_id = min(self.tweets.keys()),
+			)
+		else:
+			data = self.twitter.search.tweets(q = self.query,
+				result_type='recent', lang = 'en',
+				count = TweetFetchWorker.TWEETS_PER_REQUEST,
+			)
+
+		return data
+
+	def __saveTweets(self):
+		f = open(TweetFetchWorker.TWEETS_TEMP_FILE, "wb")
+		pickle.dump(self.tweets, f)
+		f.close()
+
+		final_data = list(self.tweets.values())
+
+		f = open(TweetFetchWorker.TWEETS_OUTPUT_FILE, "w")
+		f.write(json.dumps(final_data, sort_keys = True, indent = 4))
+		f.close()
+
+		print ("Count: {}".format(len(self.tweets)))
+
+	def __loadTweets(self):
+		if os.path.exists(TweetFetchWorker.TWEETS_TEMP_FILE):
+			f = open(TweetFetchWorker.TWEETS_TEMP_FILE, "rb")
+
+			self.tweets = pickle.load(f)
 			f.close()
-
-			print ("Count: ", len(self.tweets))
 
 if __name__ == "__main__":
 	fetch = TweetFetchWorker(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY,
